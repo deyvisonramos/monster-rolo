78416b9e4bd83950f64f543e2687b271acd34995
Deyvison Ramos <deyvisonr0@gmail.com>
2020-04-04 03:58:34 -0300

    Updates

---
+++ precache-manifest.0dc705838b20fe0a41a5b24865abfd55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc6493c2995553cb3a96106d03cfc21",
+    "url": "/moster-rolo/index.html"
+  },
+  {
+    "revision": "053721a3db6c808c64bf",
+    "url": "/moster-rolo/static/css/main.2bbc82b9.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/moster-rolo/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moster-rolo/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "053721a3db6c808c64bf",
+    "url": "/moster-rolo/static/js/main.d5bd088c.chunk.js"
+  },
+  {
+    "revision": "e4d807134057088b7b5b",
+    "url": "/moster-rolo/static/js/runtime-main.4633f2a9.js"
+  }
+]);
